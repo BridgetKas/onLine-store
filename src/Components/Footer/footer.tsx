4eb7b5cf051df060f977d19e967f1c8d99067381
BridgetKas <bridgetnamugga@gmail.com>
2025-03-29 09:24:08 +0300

    Added socials

--- src/Components/Footer/footer.tsx
+++ src/Components/Footer/footer.tsx
@@ -1,8 +1,16 @@
-
+import { FaInstagram } from "react-icons/fa";
+import { FaFacebookSquare } from "react-icons/fa";
+import { FaYoutube } from "react-icons/fa";
 function Footer() {
   return (
     <div>
+        <div>
+            <p>SOLYN.</p>
+            <a href="#"><FaInstagram/></a>
+            <a href="#"><FaFacebookSquare /></a>
+            <a href="#"> <FaYoutube /></a>
 
+        </div>
     </div>
   )
 }
